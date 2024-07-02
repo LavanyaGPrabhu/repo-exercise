377ebea49284ee19a8805dcfe6ebf5b949e04632
LavanyaGPrabhu <lavanya.prabhutwo@gmail.com>
2024-07-02 11:44:58 +0530

    Added Module 4 Exercises

--- Module4/Programming Assignment ( Writing a Unit Test)/jest-testing/timesTwo.test.js
+++ Module4/Programming Assignment ( Writing a Unit Test)/jest-testing/timesTwo.test.js
@@ -1,6 +1,6 @@
-const timesTwo = require('./timesTwo');
+const timesTwo = require("./timesTwo");
 
 // Write the first test
-test('returns the number times 2', () => {
-    expect(timesTwo(10)).toBe(20)
+test("returns the number times 2", () => {
+  expect(timesTwo(10)).toBe(20);
 });
